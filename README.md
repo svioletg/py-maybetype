6a6766ce066ed616ee0a48a7bb963a354e57a204
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2025-12-07 16:17:41 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# py-maybetype
+
+A basic implementation of a maybe/option type in Python, largely inspired by Rust's `Option`.
+This was created as part of a separate project I had been working on, but I decided to make it into
+its own package as I wanted to use it elsewhere and its scope grew. This is not meant to be a 1:1
+replication or replacement for Rust's `Option` or Haskell's `Maybe`, as it does not implement an
+additional `Some`/`Just` type, but rather just an interperetation of the idea that I feel works for
+Python.
+
+## Usage
+
+Install the package with `pip` using the repository link:
+
+```bash
+pip install git+https://github.com/svioletg/py-maybetype
+```
+
+Call the `Maybe` constructor with a `T | None` value to create a `Maybe[T]`.
+
+```python
+from maybetype.maybe import Maybe
+
+def try_int(x: str) -> int | None:
+    """Attempts to convert a string of digits into an `int`, returning `None` if not possible."""
+    try:
+        return int(x)
+    except ValueError:
+        return None
+
+num1: Maybe[int] = Maybe(try_int('5'))
+num2: Maybe[int] = Maybe(try_int('five'))
+
+print(num1.unwrap()) # 5
+print(num2.unwrap()) # (raises ValueError)
+```
+
+This example in particular can also be done with `Maybe`'s built-in `int()` class method:
+
+```python
+from maybetype import Maybe
+
+num1: Maybe[int] = Maybe.int('5')
+num2: Maybe[int] = Maybe.int('five')
+```
---
+++ maybetype/__init__.py
@@ -0,0 +1,136 @@
+from collections.abc import Callable, Iterable, Mapping, Sequence
+from typing import Any, NoReturn
+
+
+class Maybe[T]:
+    """
+    Wraps a value that may be `T` or `None`, providing methods for conditionally using that value or short-circuiting
+    to `None` without longer checks.
+    """
+    def __init__(self, val: T | None) -> None:
+        """
+        :param val: A value to wrap.
+        """
+        self.val = val
+
+    def __repr__(self) -> str:
+        return f'Maybe({self.val!r})'
+
+    def __bool__(self) -> bool:
+        return self.val is not None
+
+    def __hash__(self) -> int:
+        return self.val.__hash__()
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Maybe):
+            return False
+        return self.val == other.val
+
+    @staticmethod
+    def int(val: Any) -> 'Maybe[int]':  # noqa: ANN401
+        """
+        Attempts to convert `val` to an `int`, returning a `Maybe`-wrapped `int` if successful, or `Maybe(None)`
+        on failure.
+        """
+        try:
+            i: int = int(val)
+            return Maybe(i)
+        except ValueError:
+            return Maybe(None)
+
+    @staticmethod
+    def cat(vals: 'Iterable[Maybe[T]]') -> list[T]:
+        """
+        Takes an iterable of `Maybe` values and returns a list of only the non-`None` values.
+
+        >>> vals = [Maybe(5), Maybe(None), Maybe(10), Maybe(None)]
+        >>> assert vals.cat() == [5, 10]
+        """
+        return [i.unwrap() for i in vals if i.val is not None]
+
+    @staticmethod
+    def map[I, O](fn: 'Callable[[I], Maybe[O]]', vals: Iterable[I]) -> list[O]:
+        """Maps `fn` onto `vals`, taking the unwrapped values and discarding `Maybe(None)`s."""
+        return [i.unwrap() for i in map(fn, vals) if i.val is not None]
+
+    def unwrap(self,
+            exc: Exception | Callable[..., NoReturn] | None = None,
+            *exc_args: object,
+        ) -> T:
+        """
+        Return the wrapped value if it is not `None`, otherwise raise `ValueError`.
+
+        :param exc: The exception to raise if the wrapped value is `None`. Can be either an `Exception` object, or a
+            `Callable` which takes any arguments and does not return. If given `None`, the default behavior is to
+            raise a `ValueError` with the message `Maybe[<type>] unwrapped into None`.
+        :param exc_args: Arguments to call `exc` with, if `exc` is a `Callable`. Otherwise, this argument is not used.
+        """
+        if self.val is None:
+            if exc is None:
+                raise ValueError(f'Maybe[{T.__name__}] unwrapped into None')
+            if isinstance(exc, Callable):
+                exc(*exc_args)
+            raise exc
+        return self.val
+
+    def this_or(self, other: T) -> 'Maybe[T]':
+        """Returns the original wrapped value if not `None`, otherwise returns a `Maybe`-wrapped `other`."""
+        return self if self.val is not None else Maybe(other)
+
+    def attr[V](self, name: str, typ: type[V] | None = None, *, err: bool = False) -> 'Maybe[V]':
+        """
+        Attempts to access an attribute `name` on the wrapped object, returning another `Maybe`-wrapped value which
+        wraps the value if it exists, or `Maybe(None)` otherwise.
+
+        :param typ: Specifies the generic type of the resulting `Maybe`. Note that the potential value returned by
+            this method will not be coerced to the given type at runtime; this argument is only for typing purposes.
+        :param err: If `True`, `AttributeError` is raised instead of returning `Maybe(None)` if `name` does not exist.
+        """
+        return Maybe(getattr(self.val, name)) if err else Maybe(getattr(self.val, name, None))
+
+    def attr_or[V](self, name: str, default: V = None) -> V:
+        """
+        Similar to the `attr` method, but is guaranteed to return a non-`None` value and requires a default.
+        """
+        try:
+            return self.attr(name, err=True).unwrap()
+        except AttributeError:
+            return default
+
+    def get[V](self,
+            accessor: Any,  # noqa: ANN401
+            _typ: type[V] | None = None,
+            *,
+            err: bool = False,
+            default: V | None = None,
+        ) -> 'Maybe[V]':
+        """
+        Attempts to access an item by `accessor` on the wrapped object, assuming the wrapped value implements
+        `__getitem__`. If it does not, or if the value does not exist (list index out of range, key does not exist on a
+        dictionary, etc.), `Maybe(None)` is returned.
+
+        :param typ: Specifies the generic type of the resulting `Maybe`. Note that the potential value returned by
+            this method will not be coerced to the given type at runtime; this argument is only for typing purposes.
+        :param err: By default, `IndexError` and `KeyError` are not raised when `__getitem__` is called on the wrapped
+            value, and `Maybe(None)` is returned instead. Setting `err` to `True` allows these errors to be raised as
+            they normally would. Note that if `__getitem__` did not exist on the wrapped value in the first placed (such
+            as with `Maybe(None)`), no error is raised, and `Maybe(None)` is returned regardless.
+        :param default: Specifies an alternate value to return a `Maybe` of instead of `None`.
+        """
+        if isinstance(self.val, Sequence | Mapping):
+            try:
+                return Maybe(self.val.__getitem__(accessor))
+            except (IndexError, KeyError):
+                if err:
+                    raise
+        return Maybe(default)
+
+    def then[R](self, func: Callable[[T], R]) -> R | None:
+        """
+        Calls `func` with the wrapped value as the argument and returns its value, or returns `None` if the wrapped
+        value is `None`.
+
+        :param func: A `Callable` which takes a type of the possible wrapped value (`T`) and can return any type (`R`).
+        """
+        return func(self.val) if self.val is not None else None
