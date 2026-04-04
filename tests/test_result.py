d5e174b0eef68fbb4a8262a5fba55310328ed9dc
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-04-04 16:05:53 -0500

    Add test for Result.err()

--- tests/test_result.py
+++ tests/test_result.py
@@ -1,5 +1,6 @@
 import pytest
 
+from maybetype import Nothing, Some
 from maybetype.errors import ResultUnwrapError
 from maybetype.result import Err, Ok, Result
 
@@ -17,6 +18,8 @@ def test_equality() -> None:
     assert Err('failure') != Err('failed')
     assert Ok(1) != Err(1) != Ok(2) != Err(2)
 
+# Result methods
+
 def test_and_then() -> None:
     def func(n: int) -> Result[int, str]:
         return Ok(n * 2) if n < 10 else Err('too big!')  # noqa: PLR2004
@@ -24,6 +27,10 @@ def test_and_then() -> None:
     assert Ok(1).and_then(func) == Ok(2)
     assert Ok(10).and_then(func) == Err('too big!')
     assert Err('not a number').and_then(func) == Err('not a number')
+
+def test_to_maybe_err() -> None:
+    assert Ok(1).err() is Nothing
+    assert Err('failure').err() == Some('failure')
 
 def test_unwrap() -> None:
     assert Ok(1).unwrap() == 1
